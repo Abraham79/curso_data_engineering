1ee1555d5e56036700cecca9edce8f614cd0f0c2
Abraham García <agarciadev@gmail.com>
2024-11-16 18:56:46 +0000

    Tabla orders work in progress

--- models/staging/sql_server_dbo/stg_sql_server_dbo__orders.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__orders.sql
@@ -3,14 +3,25 @@ with
     source as (select * from {{ source("sql_server_dbo", "orders") }}),
 
     renamed as (
+        {% set valid_shipping_service = ("dhl", "fedex", "ups", "usps") %}
+        {% set valid_promo_name = ("task-force", "leverage", "Mandatory", "Digitized", "instruction set", "Optional") %}
+        
 
         select
             order_id,
+            CASE
+                WHEN shipping_service IN {{valid_shipping_service}} THEN shipping_service
+                ELSE null
+            END 
             shipping_service,
             shipping_cost,
             address_id,
             created_at,
-            promo_id,
+            CASE
+                WHEN promo_id in {{valid_promo_name}} then LOWER(promo_id)
+                ELSE null 
+            END as promo_name,
+            md5(promo_name) as promo_id,
             estimated_delivery_at,
             order_cost,
             user_id,
