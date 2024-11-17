1b64f57bebf4536f3468c4c2d141ff5ad02256d5
Abraham García <agarciadev@gmail.com>
2024-11-17 11:11:57 +0000

    query as a dict

--- models/staging/sql_server_dbo/stg_sql_server_dbo__orders.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__orders.sql
@@ -3,6 +3,11 @@ with
     source as (select * from {{ source("sql_server_dbo", "orders") }}),
 
     renamed as (
+        {% set sql_statement %}
+            select promo_id from {{ source("sql_server_dbo", "promos")}}
+        {% endset %}
+        {%- set valid_promos = dbt_utils.get_query_results_as_dict(sql_statement)%}
+
         {% set valid_shipping_service = ("dhl", "fedex", "ups", "usps") %}
         {% set valid_promo_name = ("task-force", "leverage", "Mandatory", "Digitized", "instruction set", "Optional") %}
         
@@ -18,7 +23,7 @@ with
             address_id,
             created_at,
             CASE
-                WHEN promo_id in {{valid_promo_name}} then LOWER(promo_id)
+                WHEN promo_id in {{valid_promos}} then LOWER(promo_id)
                 ELSE 'none' 
             END as promo_name,
             md5(promo_name) as promo_id,
