6c2317b02c5c08f00e26cb25029077fc0af2d403
Abraham García <agarciadev@gmail.com>
2024-11-20 17:36:25 +0000

    all tests passed

--- models/staging/sql_server_dbo/stg_sql_server_dbo__events.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__events.sql
@@ -15,10 +15,11 @@ renamed as (
         user_id,
         product_id,
         session_id,
-        created_at,
+        {{ to_utc("created_at") }} as created_at_utc,
         order_id,
-        _fivetran_deleted,
-        _fivetran_synced
+        --_fivetran_deleted,
+        {{ to_utc("_fivetran_synced") }} as insert_date_utc
+       
 
     from source
 
