ac1313288bef66e23e18004ab92d1a002147d66f
Abraham García <agarciadev@gmail.com>
2024-11-27 17:15:00 +0000

    budget

--- models/staging/sql_server_dbo/stg_sql_server_dbo__products.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__products.sql
@@ -10,9 +10,9 @@ renamed as (
 
     select
         product_id,
+        {{ tidy_string('name') }} as product_name,
         price as product_price_usd,
-        {{ tidy_string('name') }} as promo_name,
-        inventory,
+        inventory as product_stock,
         _fivetran_deleted as deleted,
         {{ to_utc("_fivetran_synced") }} as insert_date_utc
 
