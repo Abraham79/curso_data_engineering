e56b304f244a74a4018c299f3414655974f51f21
Abraham García <agarciadev@gmail.com>
2024-12-02 17:44:02 +0000

    incrementalidad

--- models/staging/google_sheets/stg_google_sheets__budget.sql
+++ models/staging/google_sheets/stg_google_sheets__budget.sql
@@ -1,3 +1,11 @@
+{{ config(
+    materialized='incremental',
+    unique_key = '_row'
+    ) 
+    }}
+
+
+
 with 
 
 source as (
@@ -13,10 +21,17 @@ renamed as (
         product_id,
         quantity,
         month({{ to_utc("month") }}) as month,
-        {{ to_utc("_fivetran_synced") }} as insert_date_utc
+        {{ to_utc("_fivetran_synced") }} as insert_date_utc,
+        _fivetran_synced
 
     from source
 
 )
 
 select * from renamed
+
+{% if is_incremental() %}
+
+  where _fivetran_synced > (select max(_fivetran_synced) from {{ this }})
+
+{% endif %}
